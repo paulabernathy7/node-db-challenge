ae15b41fdaf154a052b110bec6c9e483e776c747
Paul Abernathy <paul.abernathy7@gmail.com>
2019-08-18 20:35:08 -0400

    all GET requests completed

--- router-model/test-model.js
+++ router-model/test-model.js
@@ -1,13 +1,33 @@
 const db = require("../data/db-config");
 
 module.exports = {
-  find
-  //   findById,
-  //   FindResources,
-  //   FindTask,
+  find,
+  findById,
+  findResources,
+  findTask
   //   add
 };
 
 function find() {
   return db("projects");
+}
+
+function findById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+function findResources(id) {
+  return db("resources as r")
+    .join("projects as p", "p.id", "r.id")
+    .where("p.id", id);
+}
+
+function findTask(id) {
+  return db("tasks as t")
+    .join("projects as p", "p.id", "t.id")
+    .select("t.id", "p.project_name", "p.Description")
+
+    .where("p.id", id);
 }
--- router-model/test-router.js
+++ router-model/test-router.js
@@ -13,4 +13,59 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await Projects.findById(id);
+
+    if (project) {
+      res.json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find project with given id." });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get projects" });
+  }
+});
+
+router.get("/:id/resources", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const resources = await Projects.findResources(id);
+
+    if (resources.length) {
+      res.json(resources);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find resources for given project" });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to get resources", error: err.message });
+  }
+});
+
+router.get("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Projects.findTask(id);
+
+    if (task.length) {
+      res.json(task);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find task for given project" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get task", error: err.message });
+  }
+});
 module.exports = router;
